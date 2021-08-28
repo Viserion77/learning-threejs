331d8e72587d9e4bcbbee554e1aa48da6a847017
Jeferson Alves <49992182+Viserion77@users.noreply.github.com>
2021-08-28 11:49:29 -0300

    refactor: fix name :D

--- webpack/src/script.js
+++ webpack/src/script.js
@@ -1,18 +1,26 @@
 import './style.css'
-import * as Three from 'three'
+import * as THREE from 'three'
 
-const scene = new THREE.Scene()// Object
+const scene = new THREE.Scene()
+
+// Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)// Sizes
+scene.add(mesh)
+
+// Sizes
 const sizes = {
     width: 800,
     height: 600
-}// Camera
+}
+
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-scene.add(camera)// Renderer
+scene.add(camera)
+
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('canvas.webgl')
 })
